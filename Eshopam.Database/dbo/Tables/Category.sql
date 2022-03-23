c56e5ffc5f24d17bad99f06ad0dfa2ef3ee36d2b
becky sama <samarebecca616@gmail.com>
2022-03-23 16:50:13 +0100

    Create table user done #6 Create table category done #1

---
+++ Eshopam.Database/dbo/Tables/Category.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Category] (
+    [Id]     INT           IDENTITY (1, 1) NOT NULL,
+    [Name]   NVARCHAR (50) NOT NULL,
+    [UserId] INT           NOT NULL,
+    CONSTRAINT [PK_Category] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Category_User] FOREIGN KEY ([UserId]) REFERENCES [dbo].[User] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [IX_Category] UNIQUE NONCLUSTERED ([Name] ASC)
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Category_1]
+    ON [dbo].[Category]([Id] ASC);
+
